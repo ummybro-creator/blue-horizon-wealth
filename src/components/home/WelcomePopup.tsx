31d51447b202b2a1fb05b1a4c9dfa389b5d5e34e
ummybro-creator <ummybro@gmail.com>
2026-08-14 12:08:19 +0530

    Update WelcomePopup.tsx

--- src/components/home/WelcomePopup.tsx
+++ src/components/home/WelcomePopup.tsx
@@ -119,7 +119,7 @@ export function WelcomePopup({ isOpen, onClose }: WelcomePopupProps) {
               <span>📅</span>
               <span>
                 Launch Date:{' '}
-                <span style={{ color: ORANGE, fontWeight: 700 }}>09th Aug 2026</span>
+                <span style={{ color: ORANGE, fontWeight: 700 }}>14th Aug 2026</span>
               </span>
             </Row>
 
